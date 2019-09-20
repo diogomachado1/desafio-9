847ba3c1ee8fad2e443d11fb8aae69b33f8fa78b
Diogo Machado <diogomachado_8@hotmail.com>
2019-09-20 10:41:03 -0300

    Feat: Create and edit meetup page

---
+++ src/pages/Meetup/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  > form > textarea {
+    font-family: Helvetica, sans-serif;
+    font-size: 14px;
+    padding-top: 10px;
+    resize: none;
+    height: auto;
+  }
+`;

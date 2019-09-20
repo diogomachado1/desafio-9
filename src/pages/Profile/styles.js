f0c7b6bad678fca07a099621a33f458e55682774
Diogo Machado <diogomachado_8@hotmail.com>
2019-09-20 14:33:34 -0300

    Feat:Create Profile Page #6

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  hr {
+    border: 0;
+    height: 1px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 10px 0 20px;
+  }
+  form > button {
+    align-self: flex-end;
+  }
+`;
